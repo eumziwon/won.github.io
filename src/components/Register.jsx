67fa490fe8d12f714b29f032734ab1a66519da55
beatziwon <ziwon1986@gmail.com>
2025-07-17 12:59:56 +0900

    fix: add form code using useRef

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 회원가입 폼
 // 1. 이름
@@ -14,14 +14,31 @@ const Register = () => {
     bio: "",
   });
 
+  const countRef = useRef(0);
+  const inputRef = useRef();
+
   const onChange = (e) => {
+    countRef.current++;
     setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = () => {
+    if (input.name === "") {
+      //이름을 입력하는 DOM 요소 포커스
+      inputRef.current.focus();
+    }
   };
 
   return (
     <div>
       <div>
-        <input name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
+        <input
+          ref={inputRef}
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
       <div>
         <input name="birth" value={input.birth} onChange={onChange} type="date" />
@@ -37,6 +54,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
